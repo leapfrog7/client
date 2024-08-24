d7dad861e1545292abecb6aca55fc31c1a67a52d
Yatin1802 <leapfrog.testseries@gmail.com>
2024-08-24 22:00:56 +0530

    added 2019-20

--- src/pages/quiz/previousYear/Exam.jsx
+++ src/pages/quiz/previousYear/Exam.jsx
@@ -27,7 +27,7 @@ const Exam = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isCompleted, setIsCompleted] = useState(false);
 
-  const yearsToShow = ["2016-17", "2018"]; // Only show these years
+  const yearsToShow = ["2016-17", "2018", "2019-20"]; // Only show these years
 
   useEffect(() => {
     // Fetch available papers and their status
